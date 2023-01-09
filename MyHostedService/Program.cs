68a49b38fbf71bacd20c9006c4d66a4c5bbaa5ad
Troy Palacino <tpalacino@users.noreply.github.com>
2023-01-09 10:50:18 -0500

    Initial commit

---
+++ MyHostedService/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+// Create the builder from the WebApplication but don't call built yet
+var appBuilder = WebApplication.CreateBuilder(args);
+
+// Add the windows service class as a HostedService to the service collection on the builder
+appBuilder.Services.AddHostedService<Service>();
+
+// Build the builder to get a web application we can map endpoints to
+var app = appBuilder.Build();
+
+// Map an endpoint
+app.MapGet("/", () => "Hello World!");
+
+// Run the web application 
+app.Run();
---
+++ MyHostedService/Service.cs
@@ -0,0 +1,14 @@
+﻿public class Service : IHostedService
+{
+	public Task StartAsync(CancellationToken cancellationToken)
+	{
+		Console.WriteLine("Service Started.");
+		return Task.CompletedTask;
+	}
+
+	public Task StopAsync(CancellationToken cancellationToken)
+	{
+		Console.WriteLine("Service Stopped.");
+		return Task.CompletedTask;
+	}
+}
